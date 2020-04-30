54fb23b56c04f0b1caac76a0f5aedd32a5cceea7
Anna Bagriy <anna.bagriy9@gmail.com>
2020-05-01 00:31:36 +0300

    Finish input data generation

--- src_inverse_problem/input_data_generation.m
+++ src_inverse_problem/input_data_generation.m
@@ -1,82 +1,53 @@
 
-%---------------------------%
+tic
+
+% Constants initialization
+
+m = 32;
+
+A0 = 1;
+A1 = 1;
+A2 = 1;
+
+nu = 0.5;
+
+% Right vector functions
+g2 = @(x, y) x + y;
+q2 = @(x, y) x - y;
+
+disp([newline char(9) 'RESULTS'])
+disp(['m = ' num2str(m)])
 
 % Initialize system of linear equations
 
-matrix_initialization
+mx = DirectProblemSolver(m, g2, q2, A0, A1, A2, nu);
 
-%---------------------------%
+% Set exact domain
 
-% Read result data from file
+r = @(t) sqrt(cos(t).^2 + 0.25 * sin(t).^2);
 
-matrix_file = 'A_matrix.txt';
-vector_file = 'y_vector.txt';
+x1 = @(t) r(t) .* cos(t);
+y1 = @(t) r(t) .* sin(t);
 
-A = dlmread(matrix_file);
-y = dlmread(vector_file);
+x2 = @(t) 2 * cos(t);
+y2 = @(t) 2 * sin(t);
 
-m = (size(y, 1) - 3) / 8;
-
-%---------------------------%
+mx.SetBoundary1(x1, y1, 0);
+mx.SetBoundary2(x2, y2);
 
 % Solve system of the equations
+% Find densities and f on Г2
 
-x = A \ y;
+mx.FindDensitiesAndConstants();
+mx.FindF();
 
-% Retrieve required data
+% Plot domain
 
-a0 = x(8 * m + 1);
-a1 = x(8 * m + 2);
-a2 = x(8 * m + 3);
+tn = 0:0.01:2 * pi;
 
-fi_func_1 = zeros(2 * m, 1);
-fi_func_2 = zeros(2 * m, 1);
+plot(x1(tn), y1(tn));
+hold on;
+plot(x2(tn), y2(tn));
+hold on;
 
-psi_func_1 = zeros(2 * m, 1);
-psi_func_2 = zeros(2 * m, 1);
-
-for ii = 1:2 * m 
-    fi_func_1(ii) = x(ii);
-    fi_func_2(ii) = x(ii + 2 * m);
-
-    psi_func_1(ii) = x(ii + 4 * m);
-    psi_func_2(ii) = x(ii + 6 * m);
-end
-
-%---------------------------%
-
-% Find f on Г_2 boundary
-
-f = zeros(1, 2 * m);
-
-for ii = 1:2 * m 
-
-    f_i = a0 + a1 * x2(ii) + a2 * y2(ii);
-
-    for jj = 1:2 * m
-
-        f_i = f_i + fi_func_1(jj) * H1(2, 1, ii, jj) / (2 * m) +...
-                    fi_func_2(jj) * (H1_1(2, ii, jj) * R(abs(jj - ii), m) + H1_2(2, ii, jj) / (2 * m)) +...
-                    psi_func_1(jj) * H2(2, 1, ii, jj) / (2 * m) +...
-                    psi_func_2(jj) * (H2_1(2, ii, jj) * R(abs(jj - ii), m) + H2_2(2, ii, jj) / (2 * m));
-
-    end
-
-    f(ii) = f_i;
-end
-
-disp(['f(x) (x on Г2) = ', num2str(f)]);
-
-%---------------------------%
-
-% Write to file
-
-f_file = 'f_vector.txt';
-
-fid = fopen(f_file, 'wt');
-
-fprintf(fid, '%d\n', f);
-
-fclose(fid);
-
-%---------------------------%
+toc
